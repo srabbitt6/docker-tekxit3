69d320073a8c24dd7ade4adfb41d989e75f04057
srabbitt6 <63086658+srabbitt6@users.noreply.github.com>
2020-04-03 04:31:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-jarfile=$(cd /data && ls|grep forge-*.jar)
-cd /data && java -server "-Xms${INIT_MEM}" "-Xmx${MAX_MEM}" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar $jarfile nogui
+cd /data && java -server "-Xms${INIT_MEM}" "-Xmx${MAX_MEM}" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
